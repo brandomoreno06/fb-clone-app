ca2d20edcdbc9c153e7717c08d167a071c6c9853
Brando <brandomoreno06@outlook.com>
2021-08-07 18:51:01 +0800

    modifying live demo

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5es18GrTiQ2HJa9e8U4ulPfTlZLnIp1Q",
-  authDomain: "facebook-clone-5423d.firebaseapp.com",
-  projectId: "facebook-clone-5423d",
-  storageBucket: "facebook-clone-5423d.appspot.com",
-  messagingSenderId: "405197376306",
-  appId: "1:405197376306:web:1e5a76442ea7755597e52b",
-  measurementId: "G-G3S2FXY1W2"
+  apiKey: "AIzaSyAofuIG6J-UE9SggiDgQUtjD8eiCfZ_l1o",
+  authDomain: "clone-app-fb.firebaseapp.com",
+  projectId: "clone-app-fb",
+  storageBucket: "clone-app-fb.appspot.com",
+  messagingSenderId: "634521056243",
+  appId: "1:634521056243:web:723fc976fe049f5af69886",
+  measurementId: "G-331MW7S2ZZ"
 };
 
 
